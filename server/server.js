2492c5c1bdee7e3bf9e7653e3f862d1258e3124e
metromark <markptoledo@gmail.com>
2018-01-13 07:50:09 +0800

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb')
 
-var {mongoose} = require('./db/mongoose')
-var {Todo} = require('./models/todo')
-var {User} = require('./models/user')
+const {mongoose} = require('./db/mongoose')
+const {Todo} = require('./models/todo')
+const {User} = require('./models/user')
 
 
-var app = express();
+const app = express();
 
 const port = process.env.PORT || 3000;
 
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 
 
 app.post('/todos', (req, res) => {
-  var todo = new Todo({
+  const todo = new Todo({
     text: req.body.text
   })
 
@@ -36,7 +36,7 @@ app.get('/todos', (req, res) => {
 })
 
 app.get('/todos/:id', (req, res) => {
-  var id = req.params.id //key value pairs
+  const id = req.params.id //key value pairs
 
   if(!ObjectID.isValid(id)) {
     return res.status(404).send()
@@ -54,7 +54,7 @@ app.get('/todos/:id', (req, res) => {
 
 app.delete('/todos/:id', (req, res) => {
   
-  var id = req.params.id //key value pairs
+  const id = req.params.id //key value pairs
 
   if(!ObjectID.isValid(id)) {
     return res.status(404).send()
@@ -69,6 +69,33 @@ app.delete('/todos/:id', (req, res) => {
     res.status(400).send()
   })
 })
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  }  else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo){
+      return res.status(404).send()
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send()
+  })
+})
+
+
 
 
 app.listen(port, () => {
